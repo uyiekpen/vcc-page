eb876b14ca14f02ae2c10a635b5caafea5a86a14
unknown <uyiekpenelizabeth@gmail.com>
2025-06-12 17:12:23 +0100

    fixes

--- src/app/lib/supbaseServer.ts
+++ src/app/lib/supbaseServer.ts
@@ -1,24 +1,22 @@
-// lib/supabase/server.ts
-"use client"
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export function createSupabaseServerClient() {
-  const cookieStore = cookies();
+export async function createClient() {
+  const cookieStore = await cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async get(name: string) {
-          return (await cookieStore).get(name)?.value;
+        get(name: string) {
+          return cookieStore.get(name)?.value;
         },
-        async set(name: string, value: string, options: CookieOptions) {
-          (await cookieStore).set({ name, value, ...options });
+        set(name: string, value: string, options: any) {
+          cookieStore.set({ name, value, ...options });
         },
-        async remove(name: string, options: CookieOptions) {
-          (await cookieStore).set({ name, value: "", ...options });
+        remove(name: string, options: any) {
+          cookieStore.set({ name, value: "", ...options });
         },
       },
     }
