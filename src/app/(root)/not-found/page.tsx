358204d94076ce33eaf728575b2ee66fa7535142
unknown <uyiekpenelizabeth@gmail.com>
2025-06-23 11:03:36 +0100

    new updates

--- src/app/(root)/not-found/page.tsx
+++ src/app/(root)/not-found/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
-const page = () => {
+// app/not-found.tsx
+export default function NotFound() {
   return (
-    <div>page</div>
-  )
+    <div className="text-center py-32">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4 text-gray-500">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
 }
-
-export default page
